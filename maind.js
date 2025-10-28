97976277fa900a68a814d9d020417b19a2b70a03
Soufyan <elomranisoufyan@gmail.com>
2025-10-28 16:03:34 +0100

    timer implemetation

--- maind.js
+++ maind.js
@@ -1,1 +1,23 @@
 let score = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let timeLeft = 600;
+    const timerElement = document.getElementById('timer');
+    const timerId = setInterval(countdown, 1000);
+
+    function countdown() {
+        if (timeLeft <= 0) {
+            clearInterval(timerId);
+            timerElement.textContent = "0:00";
+            alert("Time's up! The quiz will now be submitted.");
+        } else {
+            const minutes = Math.floor(timeLeft / 60);
+            let seconds = timeLeft % 60;
+
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            timerElement.textContent = `${minutes}:${seconds}`;
+            timeLeft--;
+        }
+    }
+});
