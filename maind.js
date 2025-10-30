3db62e6d9fbed3fb0fab31ab2697b5453cf80e70
Soufyan <elomranisoufyan@gmail.com>
2025-10-30 17:19:28 +0100

    added questions content

--- maind.js
+++ maind.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (timeLeft <= 0) {
             clearInterval(timerId);
             timerElement.textContent = "0:00";
-            alert("Time's up! The quiz will now be submitted.");
+            alert("Time's up!");
         } else {
             const minutes = Math.floor(timeLeft / 60);
             let seconds = timeLeft % 60;
@@ -21,3 +21,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
